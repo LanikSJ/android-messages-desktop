8ce89719d63f4adc3334c31bdbcad490f532a719
Restyled.io <commits@restyled.io>
2026-01-17 18:16:49 +0000

    Restyled by prettier

--- patch-electron-rebuild.js
+++ patch-electron-rebuild.js
@@ -3,10 +3,24 @@ const path = require("path");
 
 // Possible paths for clang-fetcher.js
 const possiblePaths = [
-  path.join(__dirname, "node_modules", "@electron", "rebuild", "lib",
-            "clang-fetcher.js"),
-  path.join(__dirname, "node_modules", "app-builder-lib", "node_modules",
-            "@electron", "rebuild", "lib", "clang-fetcher.js"),
+  path.join(
+    __dirname,
+    "node_modules",
+    "@electron",
+    "rebuild",
+    "lib",
+    "clang-fetcher.js"
+  ),
+  path.join(
+    __dirname,
+    "node_modules",
+    "app-builder-lib",
+    "node_modules",
+    "@electron",
+    "rebuild",
+    "lib",
+    "clang-fetcher.js"
+  ),
 ];
 
 for (const filePath of possiblePaths) {
@@ -14,8 +28,10 @@ for (const filePath of possiblePaths) {
     let content = fs.readFileSync(filePath, "utf8");
 
     // Replace the import
-    content = content.replace(/import tar from 'tar';/,
-                              "import * as tar from 'tar';");
+    content = content.replace(
+      /import tar from 'tar';/,
+      "import * as tar from 'tar';"
+    );
 
     fs.writeFileSync(filePath, content);
     console.log(`Patched ${filePath}`);

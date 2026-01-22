77e04746547b6c456a3b731e02e997a0501f0595
Restyled.io <commits@restyled.io>
2026-01-22 01:02:47 +0000

    Restyled by clang-format

--- electron-builder.js
+++ electron-builder.js
@@ -1,41 +1,47 @@
 export default {
   appId: "pw.kmr.amd",
-  artifactName: (context) => `${context.packager.appInfo.productName.replace(/ /g, '-')}-v${context.packager.appInfo.version}-${context.target.os}-${context.target.arch}.${context.target.ext}`,
-  productName: "Android Messages",
-  copyright: "Copyright 2025 Kyle Rosenberg",
-  files: ["app/**/*", "resources/**/*"],
-  directories: {
-    buildResources: "resources",
-    output: "dist",
+  artifactName: (context) =>
+      `${context.packager.appInfo.productName.replace(/ /g, '-')}-v${
+          context.packager.appInfo.version}-${context.target.os}-${
+          context.target.arch}.${context.target.ext}`,
+  productName : "Android Messages",
+  copyright : "Copyright 2025 Kyle Rosenberg",
+  files : [ "app/**/*", "resources/**/*" ],
+  directories : {
+    buildResources : "resources",
+    output : "dist",
   },
-  linux: {
-    target: ["AppImage", "snap", "deb", "pacman", "rpm", "freebsd", "zip"],
-    executableName: "AndroidMessages",
-    executableArgs: [
+  linux : {
+    target : [ "AppImage", "snap", "deb", "pacman", "rpm", "freebsd", "zip" ],
+    executableName : "AndroidMessages",
+    executableArgs : [
       "--ozone-platform-hint=auto",
       "--enable-features=WaylandWindowDecorations",
       "--gtk-version=3",
     ],
-    category: "Internet",
-    desktop: {
-      entry: {
-        Name: "Android Messages Desktop",
-        StartupWMClass: "android-messages-desktop",
+    category : "Internet",
+    desktop : {
+      entry : {
+        Name : "Android Messages Desktop",
+        StartupWMClass : "android-messages-desktop",
       },
     },
   },
-  win: {
-    target: ["nsis", "portable"],
+  win : {
+    target : [ "nsis", "portable" ],
   },
-  mac: {
-    category: "public.app-category.social-networking",
-    target: { target: "default", arch: "universal" },
+  mac : {
+    category : "public.app-category.social-networking",
+    target : {target : "default", arch : "universal"},
   },
-  portable: {
-    artifactName: (context) => `${context.packager.appInfo.productName.replace(/ /g, '-')}-v${context.packager.appInfo.version}-${context.target.os}-${context.target.arch}.portable.${context.target.ext}`,
+  portable : {
+    artifactName : (context) =>
+        `${context.packager.appInfo.productName.replace(/ /g, '-')}-v${
+            context.packager.appInfo.version}-${context.target.os}-${
+            context.target.arch}.portable.${context.target.ext}`,
   },
-  nsis: {
-    allowToChangeInstallationDirectory: true,
-    oneClick: false,
+  nsis : {
+    allowToChangeInstallationDirectory : true,
+    oneClick : false,
   },
 };
